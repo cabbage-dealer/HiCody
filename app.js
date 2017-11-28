e83dcf66150f0553226f63ab7ae70e3bfed3c7c1
cabbage-dealer <34045127+cabbage-dealer@users.noreply.github.com>
2017-11-28 14:24:25 +1100

    further updates

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
+function post() {
+	var userInput=document.getElementById("userInput").value;
+		console.log(userInput);
 
+	var node=document.createElement("p");
+	var textnode=document.createTextNode(userInput);
+	node.appendChild(textnode);
+	document.getElementById("divider").appendChild(node);
+
+	document.getElementById("form").reset();
+}
