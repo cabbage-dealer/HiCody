cc04c8afde0728a4ae4e64a3137562994c6ae912
cabbage-dealer <34045127+cabbage-dealer@users.noreply.github.com>
2017-11-28 18:36:07 +1100

    loading messages fixed

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
 var messagesRef=firebase.database().ref().child("messages"); //create new ref to a location folder named messages
 
 messagesRef.on("child_added", snap => {
-	//alert(snap.val());
-	var data = snap.child("messages").val();
+	
+	var data = snap.val();
 
-	$("#divider").append("<p>" + data + "</p>");
-	console.log($("#divider").append("<p>" + data + "</p>"));
+	$("#divider").append("<p>"+data+"</p>");
+	
 });
 
 function post() {
 	var userInput=document.getElementById("userInput").value;
-		console.log(userInput);
+		
 
 	messagesRef.push().set(userInput); //create a new message in my firebase
 
-	var node=document.createElement("p");
-	var textnode=document.createTextNode(userInput);
-	node.appendChild(textnode);
-	document.getElementById("divider").appendChild(node);
+	//var node=document.createElement("p");
+	//var textnode=document.createTextNode(userInput);
+	//node.appendChild(textnode);
+	//document.getElementById("divider").appendChild(node);
 
 	document.getElementById("form").reset();
 }
