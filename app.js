9ac04b9aed8e3c07620da8158c2923cc189fe96f
cabbage-dealer <34045127+cabbage-dealer@users.noreply.github.com>
2017-11-30 13:59:16 +1100

    made logo a refresh button

--- app.js
+++ app.js
@@ -2,10 +2,10 @@
 
 var messagesRef=firebase.database().ref().child("messages"); //create new ref to a location folder named messages
 
-messagesRef.on("child_added", snap => {
+messagesRef.on("child_added", snap => { //goes to the children of where ref is and snaps
 	
 	var data = snap.val(); //sets the value of the messages into variable data
-	$("#divider").append("<p class='para'>"+data+"</p>"); 
+	$("#divider").append("<p class='para'>"+data+"</p>"); //puts the val from data into <div>
 	
 });
 
@@ -13,12 +13,12 @@ function post() {
   
 	var userInput1=document.getElementById("userInput").value; //gets value of user's input
 
-	if (/\S/.test(userInput1)) {
+	if (/\S/.test(userInput1)) { //if just empty space it wont do this)
 	
 		messagesRef.push().set(userInput1); //create a new message in my firebase
 	}
-	
-	document.getElementById("form").reset();
+
+	document.getElementById("form").reset(); //rests user input
 }
 
 
